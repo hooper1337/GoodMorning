421f6873467c613889642cad1f93a63f9f3aff00
Hugo Ferreira <hugogferreira7@gmail.com>
2022-02-27 00:11:14 +0000

    Open twitter
    The script is now opening the twitter website

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import webbrowser
 
+url = 'https://twitter.com/'
 
 username = input("Please enter your twitter email:\n")
 password = input("Please enter your twitter password: \n")
+
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser("C:\Program Files\Google\Chrome\Application\chrome.exe"))
+webbrowser.get('chrome').open(url)
+
 
 
