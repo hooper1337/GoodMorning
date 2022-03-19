de2557612276bbd32dca401712883f31e8caefaa
Hugo Ferreira <hugogferreira7@gmail.com>
2022-03-19 16:30:28 +0000

    Email
    Application is now writting the email on the field

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from lib2to3.pgen2 import driver
+from time import sleep
+from selenium.webdriver.chrome.service import Service
 import webbrowser
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
-url = 'https://twitter.com/'
 
-username = input("Please enter your twitter email:\n")
-password = input("Please enter your twitter password: \n")
+
+url = 'https://twitter.com/i/flow/login'
+
+email = input("Please enter your twitter email: ")
+print("\n")
+password = input("Please enter your twitter password: ")
 
 #instantiate the chrome class web
-driver = webdriver.Chrome(ChromeDriverManager().install())
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service = service)
+
+wait = WebDriverWait(driver, 30)
 
 #maximize the chrome windows to full-screen
 driver.maximize_window()
 
-driver.get("https://twitter.com/")
 
+#open twitter 
+driver.get(url)
 
+#application waits for the login field to be visibles
+wait.until(EC.visibility_of_element_located((By.NAME, "text")))
 
-
-
+#write the email
+driver.find_element(by=By.NAME, value='text').send_keys(email)
