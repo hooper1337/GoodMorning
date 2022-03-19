a6b94f9d2298a17ca0d6c78c18c36053a28f19da
Hugo Ferreira <hugogferreira7@gmail.com>
2022-03-19 18:33:57 +0000

    Login
    Logging in twitter is now completed

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from lib2to3.pgen2 import driver
 from time import sleep
 from selenium.webdriver.chrome.service import Service
-import webbrowser
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from keyboard import press
 
 
 
@@ -21,18 +19,30 @@ password = input("Please enter your twitter password: ")
 #instantiate the chrome class web
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service = service)
-
 wait = WebDriverWait(driver, 30)
 
 #maximize the chrome windows to full-screen
 driver.maximize_window()
 
-
 #open twitter 
 driver.get(url)
 
-#application waits for the login field to be visibles
+#application waits for the email field to be visibles
 wait.until(EC.visibility_of_element_located((By.NAME, "text")))
 
 #write the email
 driver.find_element(by=By.NAME, value='text').send_keys(email)
+
+#click on advance
+press('enter')
+
+#application waits for the email field to be visibles
+wait.until(EC.visibility_of_element_located((By.NAME, "password")))
+
+#write the password
+driver.find_element(by=By.NAME, value='password').send_keys(password)
+
+#click on login
+press('enter')
+
+sleep(10)
